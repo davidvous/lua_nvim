7608623c9a9a2f1546ff28877a87f7f1d537f018
David Le <david.le@zmacv1yjk37wqc.stationcasinos.net>
2024-08-26 23:38:50 -0700

    lsp-running

--- lua/davidvous/keymaps.lua
+++ lua/davidvous/keymaps.lua
@@ -1,12 +1,22 @@
 
 vim.g.mapleader = " "
+vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+--mappings for buffers
+vim.keymap.set("n", "<leader>bp", ":bn<CR>")
+vim.keymap.set("n", "<leader>bn", ":bp<CR>")
+
+-- swap top and bottom line
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Switches window panes
 vim.keymap.set("n", "<leader>h", ":wincmd h<CR>", { silent = true})
 vim.keymap.set("n", "<leader>j", ":wincmd j<CR>", { silent = true})
 vim.keymap.set("n", "<leader>k", ":wincmd k<CR>", { silent = true})
 vim.keymap.set("n", "<leader>l", ":wincmd l<CR>", { silent = true})
-vim.keymap.set("n", "<leader>oo", ":NvimTreeToggle<CR>")
+--vim.keymap.set("n", "<leader>oo", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>=", ":vertical resize +5<CR>", { silent = true})
 vim.keymap.set("n", "<leader>-", ":vertical resize -5<CR>", { silent = true})
 
--- lua/davidvous/lazy/nvim-tree.lua
+++ lua/davidvous/lazy/nvim-tree.lua
@@ -1,11 +1,6 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+        require("nvim-tree").setup({})
+    end,
 }
